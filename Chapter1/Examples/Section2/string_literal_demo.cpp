5226b021758331a8c5109b1067db7a77f7e2d2c1
Myrddin Krustowski <54777517+theundergroundsorcerer@users.noreply.github.com>
2022-05-31 16:35:15 +0300

    Added example of using string literal with s suffix

---
+++ Chapter1/Examples/Section2/string_literal_demo.cpp
@@ -0,0 +1,11 @@
+// Demonstrates how declaration of string literal
+#include <iostream>
+#include <typeinfo>
+using namespace std::string_literals;
+
+int main() {
+    auto name1 = "Name"s; 
+    std::cout << "name1 has type " << typeid(name1).name() << '\n';
+
+
+}
