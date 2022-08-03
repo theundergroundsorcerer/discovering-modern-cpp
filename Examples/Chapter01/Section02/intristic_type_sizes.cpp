bcd4c3ca1ad787ccf4aac545ef718ade240eda4a
Myrddin Krustowski <54777517+theundergroundsorcerer@users.noreply.github.com>
2022-08-03 16:35:18 +0300

    Finished Vector with Raw Pointers Example updated .gitignore

--- Examples/Chapter01/Section02/intristic_type_sizes.cpp
+++ Examples/Chapter01/Section02/intristic_type_sizes.cpp
@@ -5,15 +5,15 @@
 
 int main() {
     std::cout << "Sizes of built-in types on your system:\n"
-            << "Size of char is: " << sizeof(char) << " bytes\n"
-            << "Size of short is: " << sizeof(short) << " bytes\n"
-            << "Size of int is: " << sizeof(int) << " bytes\n"
-            << "Size of long is: " << sizeof(long) << " bytes\n"
-            << "Size of long long is: " << sizeof(long long) << " bytes\n"
-            << "Size of float is: " << sizeof(float) << " bytes\n"
-            << "Size of double is: " << sizeof(double) << " bytes\n"
-            << "Size of long double is: " << sizeof(long double) << " bytes\n"
-            << "Size of bool is: " << sizeof(bool) << " bytes\n"
+            << "Length of char is: " << sizeof(char) << " bytes\n"
+            << "Length of short is: " << sizeof(short) << " bytes\n"
+            << "Length of int is: " << sizeof(int) << " bytes\n"
+            << "Length of long is: " << sizeof(long) << " bytes\n"
+            << "Length of long long is: " << sizeof(long long) << " bytes\n"
+            << "Length of float is: " << sizeof(float) << " bytes\n"
+            << "Length of double is: " << sizeof(double) << " bytes\n"
+            << "Length of long double is: " << sizeof(long double) << " bytes\n"
+            << "Length of bool is: " << sizeof(bool) << " bytes\n"
             << "char type is " << (static_cast<int>(std::numeric_limits<char>::min()) >= 0 ? 
             "unsigned\n" : "signed\n");
         return 0;
--- Examples/Chapter02/VectorRawPointerV/vector_raw_pointer.cpp
+++ Examples/Chapter02/VectorRawPointerV/vector_raw_pointer.cpp
@@ -10,7 +10,7 @@ public:
     using size_type = std::initializer_list<double>::size_type;
 
 
-    explicit Vector(size_type size) : size{size}, elements{nullptr} {
+    explicit Vector(size_type size) : length{size}, elements{nullptr} {
 #ifdef PRINT_FUNCTION_MESSAGES
         // prints constructor version in debug mode - in order to demonstrate copy elision
         std::cout << "Vector(int) constructor invoked!\n";
@@ -20,35 +20,36 @@ public:
         }
     }
 
-    Vector(const Vector &source) : Vector(source.size) {
+    Vector(const Vector &source) : Vector(source.length) {
 #ifdef PRINT_FUNCTION_MESSAGES
         std::cout << "Copy constructor invoked!\n";
 #endif
         assert(source.elements != nullptr);
-        if (source.size != 0) {
-            for (size_type i = 0; i < source.size; ++i) {
+        if (source.length != 0) {
+            for (size_type i = 0; i < source.length; ++i) {
                 elements[i] = source.elements[i];
             }
         }
     }
 
     Vector &operator=(const Vector &source) & {
-#ifdef PRINT_FUNCTION_MESSAGES
+
+#ifdef PRINT_FUNCTION_MESSAGGES
         std::cout << "Move constructor called!\n";
 #endif
-        assert(source.size == 0 || source.elements != nullptr);
+        assert(source.length == 0 || source.elements != nullptr);
         if (this == &source) {
             return *this;
         }
 
-        size = 0;
+        length = 0;
         delete[] elements;
         elements = nullptr;
 
-        if (size > 0) {
-            size = source.size;
-            elements = new double[size];
-            for (size_type i = 0; i < source.size; ++i) {
+        if (length > 0) {
+            length = source.length;
+            elements = new double[length];
+            for (size_type i = 0; i < source.length; ++i) {
                 elements[i] = source.elements[i];
             }
         }
@@ -56,26 +57,26 @@ public:
         return *this;
     }
 
-    Vector(Vector &&source) noexcept : Vector(source.size)  {
+    Vector(Vector &&source) noexcept : Vector(source.length)  {
 #ifdef PRINT_FUNCTION_MESSAGES
         std::cout << "Move constructor invoked!\n";
 #endif
-        assert(source.size == 0 || source.elements != nullptr);
-        size = source.size;
+        assert(source.length == 0 || source.elements != nullptr);
+        length = source.length;
         elements = source.elements;
         source.elements = nullptr;
-        source.size = 0;
+        source.length = 0;
     }
 
     Vector &operator=(Vector &&source) & noexcept {
 #ifdef PRINT_FUNCTION_MESSAGES
         std::cout << "Move assignment invoked!\n";
 #endif
-        assert(source.size == 0 || source.elements != nullptr);
-        size = source.size;
+        assert(source.length == 0 || source.elements != nullptr);
+        length = source.length;
         elements = source.elements;
         source.elements = nullptr;
-        source.size = 0;
+        source.length = 0;
         return *this;
     }
 
@@ -92,13 +93,13 @@ public:
 #ifdef PRINT_FUNCTION_MESSAGES
         std::cout << "std::initializer_list assignment invoked\n";
 #endif
-        size = 0;
+        length = 0;
         delete[] elements;
         elements = nullptr;
 
         if (values.size() > 0) {
-            size = values.size();
-            elements = new double[size];
+            length = values.size();
+            elements = new double[length];
             std::copy(values.begin(), values.end(), elements);
         }
 
@@ -106,28 +107,31 @@ public:
     }
 
     ~Vector() {
+#ifdef PRINT_FUNCTION_MESSAGES
         std::cout << "Destructor called\n";
-        size = 0;
+#endif
+        length = 0;
         delete[] elements;
     }
 
     [[nodiscard]]
-    size_type Size() const {
-        return size;
+    size_type Length() const {
+        return length;
     }
 
     double &operator[](size_type i) &{
-        assert(i < size);
+        assert(i < length);
         assert(elements != nullptr);
         return elements[i];
     }
 
     double operator[](size_type i) const &{
-        assert(i < size);
+        assert(i < length);
         assert(elements != nullptr);
         return elements[i];
     }
 
+    [[nodiscard]]
     double Norm() const {
 #ifdef PRINT_FUNCTION_MESSAGES
         std::cout << "Norm function invoked\n";
@@ -138,71 +142,99 @@ public:
 
 
 private:
-    size_type size;
+    size_type length;
     double *elements;
     friend Vector operator+(const Vector& v1, const Vector& v2);
-    friend Vector operator*(const double c, const Vector& v);
+    friend Vector operator-(const Vector& v1, const Vector& v2);
+    friend Vector operator*(const double c, const Vector& v); // same as dot product
+    friend double operator*(const Vector& v1, const Vector& v2);
     friend std::ostream& operator<<(std::ostream& os, const Vector& v);
     friend double DotProduct(const Vector& v1, const Vector& v2);
     friend double Norm(const Vector& v);
 };
 
 inline Vector operator+(const Vector& v1, const Vector& v2) {
-    assert(v1.size == 0 || v1.elements != nullptr);
-    assert(v1.size == 0 || v2.elements != nullptr);
-    assert(v1.size == v2.size);
+    assert(v1.length == 0 || v1.elements != nullptr);
+    assert(v1.length == 0 || v2.elements != nullptr);
+    assert(v1.length == v2.length);
 
-    Vector result(v1.size);
+    Vector result(v1.length);
 
-    for(Vector::size_type i = 0; i < v1.size; ++i) {
+    for(Vector::size_type i = 0; i < v1.length; ++i) {
         result[i] = v1[i] + v2[i];
     }
 
     return result;
 }
 
-inline std::ostream& operator<<(std::ostream& os, const Vector& v) {
-    assert(v.size == 0 || v.elements != nullptr);
-    os << "(";
-    for(Vector::size_type i = 0; i < v.size-1; ++i) {
-        os << v[i] << ',';
+inline Vector operator-(const Vector& v1, const Vector& v2) {
+    assert(v1.length == 0 || v1.elements != nullptr);
+    assert(v2.length == 0 || v2.elements != nullptr);
+    assert(v1.length == v2.length);
+
+    Vector result(v1.length);
+    for(Vector::size_type i = 0; i < v1.length; ++i) {
+        result[i] = v1[i] - v2[i];
     }
 
-    if(v.size == 0) {
-        os << ')';
-        return os;
-    }
-
-    os << v[v.size-1] << '}';
-    return os;
+    return result;
 }
 
 inline Vector operator*(double c, const Vector& v) {
-    assert(v.size == 0 || v.elements != nullptr);
+    assert(v.length == 0 || v.elements != nullptr);
     Vector result{v};
-    for(Vector::size_type i = 0; i < result.size; ++i) {
+    for(Vector::size_type i = 0; i < result.length; ++i) {
         result[i] *= c;
     }
 
     return result;
 }
 
-inline double DotProduct(const Vector& v1, const Vector& v2) {
-    assert(v1.size == v2.size);
-    assert(v1.size == 0 || v1.elements != nullptr);
-    assert(v2.size == 0 || v2.elements != nullptr);
+inline double operator *(const Vector& v1, const Vector& v2) {
+    assert(v1.length == 0 || v1.elements != nullptr);
+    assert(v2.length == 0 || v2.elements != nullptr);
+    assert(v1.length == v2.length);
+
     double result = 0;
-    for(Vector::size_type i = 0; i < v1.size; ++i) {
+    for(Vector::size_type i = 0; i < v1.length; ++i) {
         result += v1[i] * v2[i];
     }
+
     return result;
 }
 
-inline double Norm(const Vector& v) {
+inline std::ostream& operator<<(std::ostream& os, const Vector& v) {
+    assert(v.length == 0 || v.elements != nullptr);
+    os << "(";
+    for(Vector::size_type i = 0; i < v.length - 1; ++i) {
+        os << v[i] << ',';
+    }
 
+    if(v.length == 0) {
+        os << ')';
+        return os;
+    }
+
+    os << v[v.length - 1] << ')';
+    return os;
+}
+
+inline double DotProduct(const Vector& v1, const Vector& v2) {
+    return v1 * v2;
+}
+
+inline double Norm(const Vector& v) {
     return v.Norm();
 }
 
 int main() {
-
+    Vector v{1,1,1,1};
+    Vector u{1, -1, 1, -1};
+    std::cout << "v = " << v << ". The length of v is " << v.Length() << '\n';
+    std::cout << "u = " << u << ". The length of u is " << u.Length() << '\n';
+    std::cout << v <<" + " << u << " = " << v + u << '\n';
+    std::cout << v << " - " << u << " = " << v - u << '\n';
+    std::cout << "<" << v <<", " << u << "> = " << DotProduct(v,u) << '\n';
+    std::cout << "|" <<v << "| = " << Norm(v) << '\n';
+    std::cout << "v + 2 * u = " << v + 2 * u << '\n';
 }
